12de62df4092ae177c86467677c8fbb51135d6a0
Pau Martin <kingdoncom@gmail.com>
2016-02-13 12:10:52 +0100

    Initial Commit

---
+++ URVNotes/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  URVNotes
+//
+//  Created by Pau Martín on 13/2/16.
+//  Copyright © 2016 Alcapone. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
