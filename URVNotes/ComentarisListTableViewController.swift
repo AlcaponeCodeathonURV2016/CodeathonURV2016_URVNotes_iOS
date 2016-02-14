c6defefb4380c30e88160ff8b6de960b8ac0ee6c
Pau Martin <kingdoncom@gmail.com>
2016-02-14 16:06:56 +0100

    comentaris d'apunts

--- URVNotes/ComentarisListTableViewController.swift
+++ URVNotes/ComentarisListTableViewController.swift
@@ -17,14 +17,6 @@ class ComentarisListTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
     override func didReceiveMemoryWarning() {
@@ -44,12 +36,12 @@ class ComentarisListTableViewController: UITableViewController {
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return self.comentaris.count
     }
 
     
@@ -74,7 +66,6 @@ class ComentarisListTableViewController: UITableViewController {
                 "likes": 0,
                 "dislikes": 0,
                 "data": NSDate()]);
-            
             
             try! realm.write {
                 realm.add(comentari)
